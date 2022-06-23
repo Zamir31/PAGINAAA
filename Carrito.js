0cd389d50100f9f9bc1dd024432492bb27f312cc
Roberto Sandoval <rs8a@icloud.com>
2022-06-22 18:08:05 -0600

    cambios

--- Carrito.js
+++ Carrito.js
@@ -1,11 +1,27 @@
 let products = [];
+let prices = [];
 let total = 0;
 
 function agregar(Producto, Precio) {
     console.log(Producto, Precio);
     products.push(Producto);
+    prices.push(Precio);
     total = total + Precio;
-    document.getElementById("total_display").value = `Pagar $${total}`
+    var html='';
+    products.forEach(element => {
+        html+='<div>'+element+'</div>';
+    });
+    document.getElementById("productos").innerHTML =html;
+
+    var html2='';
+    prices.forEach(element => {
+        html2+='<div>'+'$'+element+'</div>';
+    });
+    document.getElementById("precio").innerHTML =html2;
+
+
+    document.getElementById("total_products").value = products.length;
+    document.getElementById("total_price").value = `Pagar $${total}`
 }
 
 function pay() {
